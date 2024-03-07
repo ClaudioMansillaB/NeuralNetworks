1be5881aff1770d8c7ff41e6dfe1af0fed9ec9eb
Claudio Javier Mansilla Brito <cmansilla2016@udec.cl>
2024-03-07 08:34:39 -0300

    add dev and train

--- src/train.py
+++ src/train.py
@@ -7,3 +7,13 @@ file = os.path.join('data', 'train.csv')
 
 data = np.array(pd.read_csv(file))
 
+m,n = data.shape
+np.random.shuffle(data)
+
+data_dev = data[0:1000].T
+Y_dev = data_dev[0]
+X_dev = data_dev[1:n]
+
+data_train = data[1000:m].T
+Y_train = data_train[0]
+X_train = data_train[1:n]
